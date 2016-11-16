914d7cdf36bc5ac27b7dec64d510b4003762f7b3
vivek malik <malik_vivek@network.lilly.com>
2016-11-16 16:10:38 +0530

    adding paralleax and floating button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $( document ).ready(function(){
 
   $(".button-collapse").sideNav();
-
+  $('.parallax').parallax();
   $('ul li a').click(function(){
     $('li').removeClass("active");
     $(this).parent().addClass("active");
