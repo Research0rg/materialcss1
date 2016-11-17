2ee66f2365a5df69841a54686ba2a160f9f0196b
vivek malik <malik_vivek@network.lilly.com>
2016-11-17 13:45:21 +0530

    adding preloading splash screen

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-$( document ).ready(function(){
-
+$(document).ready(function () {
+  $('#splashscreen').fadeOut(1000);
   $(".button-collapse").sideNav();
   $('.parallax').parallax();
-  $('ul li a').click(function(){
+  $('ul li a').click(function () {
     $('li').removeClass("active");
     $(this).parent().addClass("active");
-  });  
-  
+  });
+
 });
